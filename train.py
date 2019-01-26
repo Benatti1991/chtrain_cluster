6affb3528c9c9d11df22ea7372130a0635d9ca7b
Benatti1991 <simone.benatti@studenti.unipr.it>
2019-01-26 11:46:44 +0100

    Separated script for parallel part to avoid Linux bug

--- train.py
+++ train.py
@@ -10,10 +10,10 @@ from value_function import NNValueFunction
 import scipy.signal
 from utils import Logger, Scaler
 from datetime import datetime
-import os
 import argparse
 import signal
 from multiprocessing import Pool
+import run_episode
 
 # guard in the main module to avoid creating subprocesses recursively.
 if __name__ == "__main__":
@@ -50,62 +50,7 @@ def init_gym(env_name, render):
     return env, obs_dim, act_dim
 
 
-def run_episode(env, policy, scaler):
-    """ Run single episode 
 
-    Args:
-        env: environment (object)
-        policy: policy object with sample() method
-        scaler: scaler object, scales/offsets each observation
-
-    Returns: 4-tuple of NumPy arrays
-        observes: shape = (episode len, obs_dim)
-        actions: shape = (episode len, act_dim)
-        rewards: shape = (episode len,)
-        unscaled_obs: dataset for training scaler, shape = (episode len, obs_dim)
-    """
-    obs = env.reset()  #resets whenever an episode begins
-    observes, actions, rewards, unscaled_obs = [], [], [], []
-    done = False
-    step = 0.0
-    scale, offset = scaler.get()
-    scale[-1] = 1.0  # don't scale time step feature
-    offset[-1] = 0.0  # don't offset time step feature
-   
-    while not done:
-
-        obs = obs.astype(np.float64).reshape((1, -1))  
-        obs = np.append(obs, [[step]], axis=1)  # add time step feature TODO: check if this extra state is useful
-        unscaled_obs.append(obs)
-        obs = (obs - offset) * scale  # center and scale observations TODO: check ifscaler is useful (it should be according to literature)
-        observes.append(obs)
-        action = policy.sample(obs).reshape((1, -1)).astype(np.float64)
-        actions.append(action)
-        obs, reward, done, _ = env.step(action)  #state, reward, done, info = env.step(action)
-        if not isinstance(reward, float):
-            reward = np.asscalar(reward)
-        rewards.append(reward)
-        step += 1e-3  # increments time step feature
-
-    return (np.concatenate(observes), np.concatenate(actions),
-            np.array(rewards, dtype=np.float64), np.concatenate(unscaled_obs))
-
-
-def run_parallel_episodes(arg):
-        
-        total_steps = 0
-        env_c = gym.Model(False)
-        policy = Policy(31, 8, 0.003, True)
-        scaler = Scaler(31)
-        scaler.resume()
-        observes, actions, rewards, unscaled_obs = run_episode(env_c, policy, scaler)
-        total_steps += observes.shape[0]
-        trajectory = {'observes': observes,
-                      'actions': actions,
-                      'rewards': rewards,
-                      'unscaled_obs': unscaled_obs}
-        policy.close_sess()
-        return trajectory
 
 def run_policy(env, policy, scaler, logger, episodes):
     """ Run policy and collect data 
@@ -124,7 +69,7 @@ def run_policy(env, policy, scaler, logger, episodes):
         'unscaled_obs' : NumPy array of (un-scaled) states from episode
     """
 
-    trajectories = pool.map(run_parallel_episodes, range(episodes))
+    trajectories = pool.map(run_episode.run_parallel_episodes, range(episodes))
     
     unscaled = np.concatenate([t['unscaled_obs'] for t in trajectories])
     scaler.update(unscaled)  # update running statistics for scaling observations
