8ebe0f67e7b1e14b6215be0d5b7e17a145d7eac7
Simone Benatti <simone.benatti@studenti.unipr.it>
2019-04-25 10:38:23 +0200

    Reshaped reward

--- envs/chtrain_ComauR3.py
+++ envs/chtrain_ComauR3.py
@@ -281,16 +281,16 @@ class Model(object):
        
               electricity_cost     = 0.001    # cost for using self.motors -- this parameter should be carefully tuned against reward for making progress, other values less improtant
               #stall_torque_cost    = -0.1    # cost for running electric current through a motor even at zero rotational speed, small
-              
+              dist_coeff = 1
               #joints_at_limit_cost = -0.2    # discourage stuck joints
               
               #power_cost  = electricity_cost  * float(np.abs(self.ac*self.q_dot_mot).mean())  # let's assume we have DC motor with controller, and reverse current braking. BTW this is the formula of motor power
               #Reduced stall cost to avoid joints at limit
               #joints_limit = joints_at_limit_cost * self.joint_at_limit
-              self.self_coll =  1 if self.base.GetContactForce().Length()+ self.biceps.GetContactForce().Length() + self.forearm.GetContactForce().Length() == 0 else -100000
+              self.self_coll =  0 if self.base.GetContactForce().Length()+ self.biceps.GetContactForce().Length() + self.forearm.GetContactForce().Length() == 0 else -1000
               #progress = self.calc_progress()
               self.dist = np.linalg.norm([self.grip-self.targ])
-              rew = -self.dist + self.self_coll - electricity_cost*np.linalg.norm(self.ac)
+              rew = ( dist_coeff/(self.dist+0.0001)) + self.self_coll - electricity_cost*np.linalg.norm(self.ac)
               return rew
 
        """
