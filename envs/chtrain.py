690c31b986904391605f1f94b2bab25a0cb93562
Simone Benatti <simone.benatti@studenti.unipr.it>
2019-06-28 16:02:36 +0200

    Bug fix

--- envs/chtrain.py
+++ envs/chtrain.py
@@ -8,16 +8,12 @@ Created on Thu Jan 10 11:01:21 2019
 import chtrain_ant
 import chtrain_pendulum
 import chtrain_ComauR3
-import chtrain_ant_defsoil
 import chtrain_ABB_IRB120
 
        
 def Init(env_name, render):
        if env_name=='ChronoAnt':
               return chtrain_ant.Model(render)
-                     
-       elif env_name=='ChronoAntDefSoil':
-              return chtrain_ant_defsoil.Model(render)
 
        elif env_name=='ChronoPendulum':
               return chtrain_pendulum.Model(render)
